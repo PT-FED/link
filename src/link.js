a6c15a0e8f9abc3301b8759cf3ab6e1f5cc70fa9
gang.li <ligang@ptthink.com>
2016-10-28 12:27:48 +0800

    增加事件机制,优化动画嵌入方式

--- src/link.js
+++ src/link.js
@@ -17,7 +17,7 @@
     // 常量
     var VERSION = "1.0.0",
 
-        BEHAVE = {
+        STATUS = {
             VISIBLE: "visible",
             HIDDEN: "hidden",
             TOGGLE: "toggle"
@@ -33,7 +33,8 @@
          */
         LINK_ATTR_TEMPLATE = "tpl",
         LINK_ATTR_CONTAINER = "container",
-        LINK_ATTR_STATUS = "status",  // "visible","hidden","toggle",function(){}
+        LINK_ATTR_STATUS = "status",  // "visible","hidden","toggle"
+        LINK_ATTR_ANIMATE = "animate",    // 动画
         LINK_ATTR_ACTION = "action",
         LINK_ATTR_GROUP = "group",
 
@@ -62,9 +63,6 @@
                 }
             }
             return targetObj;
-        },
-        isFunction: function(obj){
-            return Object.prototype.toString.call(obj) === "[object Function]";
         },
         isEmpty: function(obj){
             return obj === null || obj === undefined;
@@ -115,18 +113,22 @@
             return {
                 template: view.attr(node, LINK_ATTR_TEMPLATE),
                 container: view.attr(node, LINK_ATTR_CONTAINER),
-                behave: view.attr(node, LINK_ATTR_STATUS) || BEHAVE.VISIBLE,
+                status: view.attr(node, LINK_ATTR_STATUS) || STATUS.VISIBLE,
+                animate: view.attr(node, LINK_ATTR_ANIMATE), // "" 区别 null
                 action: view.attr(node, LINK_ATTR_ACTION) || EMPTY_FN,
                 group: view.attr(node, LINK_ATTR_GROUP)
             }
         },
-        behave: function(targetNode, behave){
-            if(util.isFunction(behave)) return behave();
-            var displayBehave = BEHAVE[behave.toUpperCase()];
-            if(displayBehave === BEHAVE.TOGGLE){
-                displayBehave = (targetNode.style.visibility === BEHAVE.VISIBLE ? BEHAVE.HIDDEN : BEHAVE.VISIBLE); // 注意""
+        status: function(targetNode, status){
+            if(!status || !STATUS[status.toUpperCase()]) return;
+            var displayBehave = STATUS[status.toUpperCase()];
+            if(displayBehave === STATUS.TOGGLE){
+                displayBehave = (targetNode.style.visibility === STATUS.VISIBLE ? STATUS.HIDDEN : STATUS.VISIBLE); // 注意""
             }
             targetNode.style.visibility = displayBehave;
+        },
+        animate: function(targetNode, animate, content){
+            Event.trigger("link.animate", {targetNode: targetNode, animate: animate, content: content});
         },
         /**
          * @REVISIT
@@ -161,6 +163,68 @@
         }
     };
 
+    var Event = (function(){
+        // cache = {"link.animate": [{fn: fn1, data: data1}, {fn: fn2, data: data2}]}
+        var _caches = {},
+            _listen,
+            _trigger,
+            _remove;
+
+        _listen = function(keys, fn, data){
+            // 支持多个事件，共享一个处理函数
+            // 多个事件使用“逗号、空格、分号”间隔
+            var keyAry = keys.split(/[\,\s\;]/);
+            var index = keyAry.length;
+            while (index){
+                index--;
+                var key = keyAry[index];
+                if(!_caches[key]){
+                    _caches[key] = [];
+                }
+                _caches[key].push({fn: fn, data: data});
+            }
+        };
+        _remove = function(key, fn){
+            if(_caches[key]){
+                if(fn){
+                    for(var i = 0, len = _caches[key].length; i < len; i++){
+                        if(_caches[key][i].fn === fn){
+                            _caches[key].splice(i, 1);
+                        }
+                    }
+                }else{
+                    _caches[key] = [];
+                }
+            }
+        };
+        /**
+         * 触发顺序：A.B.C ==> A.B ==> A
+         */
+        _trigger = function(key, args){
+            var event,
+                aimKey = key,
+                keyAry = key.split(".");
+            for(var i = 0, len = keyAry.length; i < len; i++) {
+                event = _caches[aimKey];
+                if (event) {
+                    var j = 0, length = event.length;
+                    while (j < length) {
+                        event[j].fn(args);
+                        j++;
+                    }
+                }
+                keyAry.pop();
+                aimKey = keyAry.join(".");
+            }
+        };
+
+        return {
+            listen: _listen,
+            remove: _remove,
+            trigger: _trigger
+        };
+    })();
+
     var handleEvent = {
         init: function () {
             this.unbind();
@@ -183,10 +247,14 @@
 
                     for (var i = 0, len = targetNodes.length; i < len; i++) {
                         targetNode = targetNodes[i];
-                        view.html(targetNode, content);
-                        view.behave(targetNode, linkDom.behave);
+                        if(linkDom.animate !== null){
+                            view.animate(targetNode, linkDom.animate, content);
+                        }else{
+                            view.html(targetNode, content);
+                        }
+                        view.status(targetNode, linkDom.status);
                         view.group(targetNode, node, linkDom.group);
-                        linkDom.action(targetNode);    // action封装第一个参数为"目标元素"
+                        eval("(" + linkDom.action + ")")(targetNode);    // action封装第一个参数为"目标元素"
                     }
                 });
             }
@@ -212,17 +280,19 @@
     });
 
 
-    function init(config) {
-        var link = new Link(config);
+    function init() {
+        var link = new Link();
+        link.version = VERSION;
+        link.destroy = destroy;
+        // 事件相关
+        link.listen = link.on = Event.listen;
+        link.remove = link.off = Event.remove;
+        link.trigger = link.emit = Event.trigger;
         return link;
     }
     function destroy(link) {
         link.destroy();
     }
 
-    return {
-        version: VERSION,
-        init: init,
-        destroy: destroy
-    };
+    return init();
 }));
